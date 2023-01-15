186c6f4eca81a7dfb181e458d71047e9d706bdbd
chaloemchai-beer <74236242+chaloemchai-beer@users.noreply.github.com>
2023-01-15 14:10:12 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default function Home({ products }) {
       <Head>
         <title>Amazon Clone</title>
         <link rel="icon" href="https://merivis.org/wp-content/uploads/2018/02/Amazon-Logo-Transparent-PNG.png" />
+        <meta name="google-site-verification" content="QSaO1J-iF9KQwXjmcKfwscxLcl2kqDRe63koJ23H160" />
       </Head>
 
       <Header />
